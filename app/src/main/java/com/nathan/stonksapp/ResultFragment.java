0280739c9c4faa96afdce315d3c850d4dd4054f2
nathangaut <nathan.gaut@target.com>
2019-11-22 16:47:25 -0600

    Results Table Layout

--- app/src/main/java/com/nathan/stonksapp/ResultFragment.java
+++ app/src/main/java/com/nathan/stonksapp/ResultFragment.java
@@ -36,7 +36,22 @@ public class ResultFragment extends Fragment {
     private TextView mSymbol;
     private TextView mPrice;
     private TextView mLastUpdated;
+    private TextView mOpenLabel;
     private TextView mOpen;
+    private TextView mHighLabel;
+    private TextView mHigh;
+    private TextView mLowLabel;
+    private TextView mLow;
+    private TextView mVolumeLabel;
+    private TextView mVolume;
+    private TextView mLatestTradingDayLabel;
+    private TextView mLatestTradingDay;
+    private TextView mPreviousCloseLabel;
+    private TextView mPreviousClose;
+    private TextView mChangeLabel;
+    private TextView mChange;
+    private TextView mChangePercentLabel;
+    private TextView mChangePercent;
 
     //Logging thing
     private static final String TAG = "RESULT_STONK";
@@ -99,8 +114,22 @@ public class ResultFragment extends Fragment {
         mSymbol = view.findViewById(R.id.symbol);
         mPrice = view.findViewById(R.id.price);
         mLastUpdated = view.findViewById(R.id.last_updated);
+        mOpenLabel = view.findViewById(R.id.open_label);
         mOpen = view.findViewById(R.id.open);
-
+        mHighLabel = view.findViewById(R.id.high_label);
+        mHigh = view.findViewById(R.id.high);
+        mLowLabel = view.findViewById(R.id.low_label);
+        mLow = view.findViewById(R.id.low);
+        mVolumeLabel = view.findViewById(R.id.volume_label);
+        mVolume = view.findViewById(R.id.volume);
+        mLatestTradingDayLabel = view.findViewById(R.id.latest_trading_day_label);
+        mLatestTradingDay = view.findViewById(R.id.latest_trading_day);
+        mPreviousCloseLabel = view.findViewById(R.id.previous_close_label);
+        mPreviousClose = view.findViewById(R.id.previous_close);
+        mChangeLabel = view.findViewById(R.id.change_label);
+        mChange = view.findViewById(R.id.change);
+        mChangePercentLabel = view.findViewById(R.id.change_percent_label);
+        mChangePercent = view.findViewById(R.id.change_percent);
 
         //TODO: Create a button to save to favorites
 
@@ -147,11 +176,29 @@ public class ResultFragment extends Fragment {
         });
     }
 
+    //TODO: create strings in strings.xml for these
+    //TODO: create styles for text size and margin
+
     private void setTexts(final Symbol mSymbolResponse){
         mSymbol.setText(mSymbolResponse.globalQuote.symbol);
         mPrice.setText(mSymbolResponse.globalQuote.price);
-        mLastUpdated.setText("");
+        mOpenLabel.setText("Open: ");
         mOpen.setText(mSymbolResponse.globalQuote.open);
+        mHighLabel.setText("High: ");
+        mHigh.setText(mSymbolResponse.globalQuote.high);
+        mLowLabel.setText("Low: ");
+        mLow.setText(mSymbolResponse.globalQuote.low);
+        mVolumeLabel.setText("Volume: ");
+        mVolume.setText(mSymbolResponse.globalQuote.volume);
+        mLatestTradingDayLabel.setText("Latest Trading Day: ");
+        mLatestTradingDay.setText(mSymbolResponse.globalQuote.latestTradingDay);
+        mPreviousCloseLabel.setText("Previous Close: ");
+        mPreviousClose.setText(mSymbolResponse.globalQuote.previousClose);
+        mChangeLabel.setText("Change: ");
+        mChange.setText(mSymbolResponse.globalQuote.change);
+        mChangePercentLabel.setText("Change Percent: ");
+        mChangePercent.setText(mSymbolResponse.globalQuote.changePercent);
+        mLastUpdated.setText("Last Updated: 2019-11-22");
     }
 
 }
