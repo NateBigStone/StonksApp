2a34c2fd239f58d8c4061acea19ce5c662b69163
nathangaut <nathan.gaut@target.com>
2019-11-21 21:58:00 -0600

    Fragment Work

--- app/src/main/java/com/nathan/stonksapp/MainActivity.java
+++ app/src/main/java/com/nathan/stonksapp/MainActivity.java
@@ -7,18 +7,10 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import okhttp3.OkHttpClient;
-import okhttp3.logging.HttpLoggingInterceptor;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.jackson.JacksonConverterFactory;
 
 public class MainActivity extends AppCompatActivity{
 
@@ -27,11 +19,7 @@ public class MainActivity extends AppCompatActivity{
     private Button mSearchButton;
     private EditText mEnterSymbol;
 
-    //Database things
-    private SymbolService mSymbolService;
-    private StockViewModel mStockDatabase;
-
-    // Create the purple frag
+    // Create the frag
     private ResultFragment mResultFragment;
 
     @Override
@@ -39,27 +27,10 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Retrofit Debugging
-        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
-        logging.setLevel(HttpLoggingInterceptor.Level.BASIC);
-        OkHttpClient client = new OkHttpClient.Builder()
-                .addInterceptor(logging)
-                .build();
-
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(BuildConfig.BASE_URL)
-                .addConverterFactory(JacksonConverterFactory.create())
-                .client(client)
-                .build();
-
-        mStockDatabase = new StockViewModel(getApplication());
-        mSymbolService = retrofit.create(SymbolService.class);
-
         mSearchButton = findViewById(R.id.search_button);
         mEnterSymbol = findViewById(R.id.enter_symbol);
 
-        mResultFragment = ResultFragment.newInstance("");
-        //mResultFragment.setOnStockListener(this);
+        mResultFragment = ResultFragment.newInstance();
         FragmentManager fm = getSupportFragmentManager();
         FragmentTransaction ft = fm.beginTransaction();
         ft.add(R.id.action_fragment, mResultFragment);
@@ -74,55 +45,18 @@ public class MainActivity extends AppCompatActivity{
                     Toast.makeText(MainActivity.this, "Enter a Symbol", Toast.LENGTH_SHORT).show();
                     return;
                 }
-
                 hideKeyboard();
-                getPrice(mSymbol);
+                onSendStock(mSymbol);
+                mEnterSymbol.setText(R.string.empty);
+
             }
         });
     }
 
-    private void getPrice(final String mSymbol) {
-        String mQuery = "GLOBAL_QUOTE";
-        mSymbolService.getPrice(mQuery, mSymbol, BuildConfig.API_KEY).enqueue(new Callback<Symbol>() {
-            @Override
-            public void onResponse(@NonNull Call<Symbol> call, @NonNull Response<Symbol> response) {
-                Symbol mSymbolResponse = response.body();
-                if (mSymbolResponse != null) {
-
-                    //Save to database
-                    Stock mNewStock = new Stock(
-                            mSymbolResponse.globalQuote.symbol,
-                            mSymbolResponse.globalQuote.open,
-                            mSymbolResponse.globalQuote.high,
-                            mSymbolResponse.globalQuote.low,
-                            mSymbolResponse.globalQuote.price,
-                            mSymbolResponse.globalQuote.volume,
-                            mSymbolResponse.globalQuote.latestTradingDay,
-                            mSymbolResponse.globalQuote.previousClose,
-                            mSymbolResponse.globalQuote.change,
-                            mSymbolResponse.globalQuote.changePercent);
-                    mStockDatabase.insert(mNewStock);
-                    //TODO: Remove Log.d
-                    Log.d(TAG, "The database is: " + mStockDatabase);
-                    Toast.makeText(MainActivity.this,mSymbolResponse.globalQuote.symbol + " saved to favorites", Toast.LENGTH_LONG).show();
-
-                }
-                else {
-                    Toast.makeText(MainActivity.this,"Unable to get information", Toast.LENGTH_LONG).show();
-                }
-            }
-            @Override
-            public void onFailure(Call<Symbol> call, Throwable t) {
-                Log.e(TAG, "Error getting info", t);
-                Toast.makeText(MainActivity.this,"Unable to get information", Toast.LENGTH_LONG).show();
-            }
-        });
-    }
-
-    public void onStock(String mStockResponse) {
+    public void onSendStock(String mStock) {
         FragmentManager fm = getSupportFragmentManager();
         FragmentTransaction ft = fm.beginTransaction();
-        ResultFragment mResultFragment = ResultFragment.newInstance("");
+        ResultFragment mResultFragment = ResultFragment.newInstance(mStock);
         ft.replace(R.id.action_fragment, mResultFragment);
         ft.addToBackStack(null);
         ft.commit();
--- app/src/main/java/com/nathan/stonksapp/ResultFragment.java
+++ app/src/main/java/com/nathan/stonksapp/ResultFragment.java
@@ -1,22 +1,59 @@
 package com.nathan.stonksapp;
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.jackson.JacksonConverterFactory;
+
+//TODO: Send response to fragment
+//TODO: Be able to send Fragment empty string
+//TODO: Loading bar for fragment
+//TODO: Add to favorites button
+
 
 public class ResultFragment extends Fragment {
 
+    //Arg things
     private final static String ARG_RESPONSE = "response";
-    private String mStockResponse = "";
+    private String mStockTicker;
 
-    TextView mResponse;
+    //Element things
+    private TextView mSymbol;
+    private TextView mPrice;
+    private TextView mLastUpdated;
+    private TextView mOpen;
+
+    //Logging thing
+    private static final String TAG = "RESULT_STONK";
+
+    //Database things
+    private SymbolService mSymbolService;
+    private StockViewModel mStockDatabase;
+
 
     public ResultFragment() {
         // Required empty public constructor
     }
+
+    public static ResultFragment newInstance(){
+        return new ResultFragment();
+    }
+
 
     public static ResultFragment newInstance(String mStockResponse) {
         ResultFragment fragment = new ResultFragment();
@@ -30,10 +67,27 @@ public class ResultFragment extends Fragment {
     public void onCreate(Bundle savedInstance){
         super.onCreate(savedInstance);
 
-        if(getArguments() != null) {
-            mStockResponse = getArguments().getString(ARG_RESPONSE);
+        //Retrofit Debugging
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BASIC);
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .build();
 
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BuildConfig.BASE_URL)
+                .addConverterFactory(JacksonConverterFactory.create())
+                .client(client)
+                .build();
+
+        mStockDatabase = new StockViewModel(getActivity().getApplication());
+        mSymbolService = retrofit.create(SymbolService.class);
+
+        if(getArguments() != null) {
+            mStockTicker = getArguments().getString(ARG_RESPONSE);
+            getPrice(mStockTicker);
         }
+
     }
 
     @Override
@@ -42,9 +96,62 @@ public class ResultFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.result_fragment, container, false);
         // Inflate the layout for this fragment
-        mResponse = view.findViewById(R.id.response);
-        mResponse.setText(mStockResponse);
+        mSymbol = view.findViewById(R.id.symbol);
+        mPrice = view.findViewById(R.id.price);
+        mLastUpdated = view.findViewById(R.id.last_updated);
+        mOpen = view.findViewById(R.id.open);
+
+
+        //TODO: Create a button to save to favorites
+
         return view;
+    }
+
+    private void getPrice(final String mSymbol) {
+        String mQuery = "GLOBAL_QUOTE";
+        mSymbolService.getPrice(mQuery, mSymbol, BuildConfig.API_KEY).enqueue(new Callback<Symbol>() {
+            @Override
+            public void onResponse(@NonNull Call<Symbol> call, @NonNull Response<Symbol> response) {
+                Symbol mSymbolResponse = response.body();
+                if (mSymbolResponse != null) {
+                    //Set the texts
+                    setTexts(mSymbolResponse);
+
+                    //Save to database
+                    Stock mNewStock = new Stock(
+                            mSymbolResponse.globalQuote.symbol,
+                            mSymbolResponse.globalQuote.open,
+                            mSymbolResponse.globalQuote.high,
+                            mSymbolResponse.globalQuote.low,
+                            mSymbolResponse.globalQuote.price,
+                            mSymbolResponse.globalQuote.volume,
+                            mSymbolResponse.globalQuote.latestTradingDay,
+                            mSymbolResponse.globalQuote.previousClose,
+                            mSymbolResponse.globalQuote.change,
+                            mSymbolResponse.globalQuote.changePercent);
+                    mStockDatabase.insert(mNewStock);
+                    //TODO: Remove Log.d
+                    Log.d(TAG, "The database is: " + mStockDatabase);
+                    Toast.makeText(getContext(),mSymbolResponse.globalQuote.symbol + " saved to favorites", Toast.LENGTH_LONG).show();
+
+                }
+                else {
+                    Toast.makeText(getContext(),"Unable to get information", Toast.LENGTH_LONG).show();
+                }
+            }
+            @Override
+            public void onFailure(Call<Symbol> call, Throwable t) {
+                Log.e(TAG, "Error getting info", t);
+                Toast.makeText(getContext(),"Unable to get information", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void setTexts(final Symbol mSymbolResponse){
+        mSymbol.setText(mSymbolResponse.globalQuote.symbol);
+        mPrice.setText(mSymbolResponse.globalQuote.price);
+        mLastUpdated.setText("");
+        mOpen.setText(mSymbolResponse.globalQuote.open);
     }
 
 }
